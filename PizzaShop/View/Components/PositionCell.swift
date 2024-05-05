bc6e6275470471ccbc556527302df49b9a65773b
Bidzhou <89260226211@mail.ru>
2024-05-05 21:31:19 +0300

    recreated card, add some features

---
+++ PizzaShop/View/Components/PositionCell.swift
@@ -0,0 +1,18 @@
+//
+//  PositionCell.swift
+//  PizzaShop
+//
+//  Created by Bit Bitin on 5.05.2024.
+//
+
+import SwiftUI
+
+struct PositionCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PositionCell()
+}
